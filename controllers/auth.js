1a1d3262cf2babf220abfe1085528bcb31b52adf
mario mora <jorgemariomoramolina@gmail.com>
2024-12-30 21:56:50 -0500

    true final commit

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,13 +6,13 @@ const User = require('../models/user');
 const { DATE } = require('sequelize');
 
 
-const transporter = nodemailer.createTransport(
+/* const transporter = nodemailer.createTransport(
     sendgridTransport({
         auth: {
             api_key: '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'
         }
     })
-);
+); */
 
 exports.getLogin = (req, res, next) => {
     let message = req.flash('error');
